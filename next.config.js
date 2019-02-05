55580dcad96717db257680af3bc396ae938ac77b
jake manx <jacobzadelman@gmail.com>
2019-02-05 18:30:36 -0500

    added serverless + domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withCSS = require("@zeit/next-css");
+// const getRoutes = require("./routes");
 
 module.exports = withCSS({
+  // exportPathMap: getRoutes
   /* my next config */
   // cssModules: true
 });
---
+++ routes.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+  return {
+    "/": { page: "/" },
+    "/login": { page: "/login" },
+    "/register": { page: "/register" }
+  };
+};
